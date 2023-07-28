d584e20b5173c2a8ccf23159f053c3a04325e26d
lRomanti01 <lRomanti01@users.noreply.github.com>
2023-07-27 21:30:33 -0300

    Fixing a export problem

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -12,7 +12,9 @@ const Proyects = () => {
       id="projects"
       className="scroll-mt-32 text-black dark:text-white flex flex-col items-center mt-5 min-h-screen"
     >
-      <h1 className="uppercase font-bold text-2xl md:text-4xl mb-10">PROJECTS</h1>
+      <h1 className="uppercase font-bold text-2xl md:text-4xl mb-10">
+        PROJECTS
+      </h1>
       <div className="grid p-10 md:gap-10 md:grid-cols-2 xl:grid-cols-3">
         <ProjectTemplate
           image={TodoApp}
@@ -42,4 +44,4 @@ const Proyects = () => {
   );
 };
 
-export default Proyects;
+export default Projects;
